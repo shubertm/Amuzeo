172100707052723958dd6d16e7f8dcc2b3ae2956
Shubert Munthali <munthalimartinz@gmail.com>
2024-11-08 16:29:03 +0200

    Fix ktlint errors in build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,11 @@ var pass: String = ""
 Properties().apply {
     val file = rootDir.resolve("github.properties")
     if (file.exists()) {
-        load(
-            FileInputStream(file)
-        )
+        load(FileInputStream(file))
         user = this["gpr.usr"].toString()
         pass = this["gpr.key"].toString()
     }
 }
-
 
 pluginManagement {
     repositories {
@@ -41,7 +38,9 @@ dependencyResolutionManagement {
             url = URI.create("https://maven.pkg.github.com/ARK-Builders/arklib-android")
             credentials {
                 username = "token"
-                password = "\u0037\u0066\u0066\u0036\u0030\u0039\u0033\u0066\u0032\u0037\u0033\u0036\u0033\u0037\u0064\u0036\u0037\u0066\u0038\u0030\u0034\u0039\u0062\u0030\u0039\u0038\u0039\u0038\u0066\u0034\u0066\u0034\u0031\u0064\u0062\u0033\u0064\u0033\u0038\u0065"
+                password = "\u0037\u0066\u0066\u0036\u0030\u0039\u0033\u0066\u0032\u0037\u0033\u0036\u0033\u0037" +
+                    "\u0064\u0036\u0037\u0066\u0038\u0030\u0034\u0039\u0062\u0030\u0039\u0038\u0039\u0038\u0066\u0034" +
+                    "\u0066\u0034\u0031\u0064\u0062\u0033\u0064\u0033\u0038\u0065"
             }
         }
     }
